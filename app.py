d7067b3a7e9a33c7daa33ec713cc4cbdf37c81b4
Anil-Tejas <aniltejas123@gmail.com>
2025-12-30 08:26:39 +0000

    Update app.py

--- app.py
+++ app.py
@@ -59,6 +59,7 @@ if check:
         st.warning("Please type something first.")
     else:
         found = False
+st.write("**relax cutie baby chin made me precisely for this**")
 
         for key, advice in solutions.items():
             if key in text:
@@ -70,5 +71,6 @@ if check:
 
 
 st.caption("⚠️ Not medical advice. If pain is severe, persistent, or worrying, see a doctor.")
+
 
 
